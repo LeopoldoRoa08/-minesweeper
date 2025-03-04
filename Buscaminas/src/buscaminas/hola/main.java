b567b98969b0e0ef290fe55315b425c390d2712b
ricardo <r.fernandez@correo.unimet.edu.ve>
2025-03-04 13:48:54 -0400

    cambios en los grafos
    ds

---
+++ Buscaminas/src/buscaminas/hola/main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package buscaminas.hola;
+
+/**
+ *
+ * @author zarna
+ */
+public class main {
+    public static void main(String[] args){
+    GrafoLA grafo = new GrafoLA();
+    Casilla c = new Casilla(3,2);
+    Casilla e = new Casilla(4,2);
+    
+    grafo.Append(c);
+    grafo.Append(e);
+    grafo.imprimirCasillas();
+    System.out.println(grafo.getPfirst().getProw());
+    
+    }
+}
