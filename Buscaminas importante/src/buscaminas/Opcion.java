454b76dd3c6723d72f7d6370b68368be22bc9377
Miguel Ignazio Greco Aponte <167722668+ConditionalFinality@users.noreply.github.com>
2025-03-07 15:58:00 -0400

    Boton para crear archivo de texto.
    De momento solo tiene un identificador y luego una placeholder que dice "LEBRON!". Implementado usando BufferedWriter, JFileChooser y FileWriter.

--- Buscaminas importante/src/buscaminas/Opcion.java
+++ Buscaminas importante/src/buscaminas/Opcion.java
@@ -116,11 +116,11 @@ public class Opcion extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 432, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 293, Short.MAX_VALUE)
         );
 
         pack();
